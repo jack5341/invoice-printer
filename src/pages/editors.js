e94cd68f1f7c3e4d4ab56256adb8827aa5a065ef
jack5341 <nedim.akar53411@gmail.com>
2021-03-06 13:17:17 +0100

    fix: without query string error on edit page

--- src/pages/editors.js
+++ src/pages/editors.js
@@ -25,55 +25,59 @@ export default function Editors() {
     window.localStorage.getItem("configuration_settings")
   );
 
-  return (
-    <Container padding="2rem" maxW="4xl">
-      <Alert className="alert" mb="5" width="21cm">
-        <EditIcon mr="5" />
-        <Box flex="1">
-          <AlertTitle>Editable !</AlertTitle>
-          <AlertDescription display="block">
-            In A4 Paper everything is editable what you see,go then try it.
-            <br />
-            Your inputs will appear in tooltip while your mouse on over the
-            text.
-          </AlertDescription>
+  if (queryObject) {
+    return (
+      <Container padding="2rem" maxW="4xl">
+        <Alert className="alert" mb="5" width="21cm">
+          <EditIcon mr="5" />
+          <Box flex="1">
+            <AlertTitle>Editable !</AlertTitle>
+            <AlertDescription display="block">
+              In A4 Paper everything is editable what you see,go then try it.
+              <br />
+              Your inputs will appear in tooltip while your mouse on over the
+              text.
+            </AlertDescription>
+          </Box>
+          <CloseButton
+            onClick={() =>
+              (document.querySelector(".alert").style = "display:none")
+            }
+            position="absolute"
+            right="8px"
+            top="8px"
+          />
+        </Alert>
+        <Box
+          color="black"
+          padding="3rem"
+          background={colorMode === "light" ? "whitesmoke" : "white"}
+          width="21cm"
+          height="29cm"
+        >
+          <SimpleGrid fontWeight="500" columns={2} spacing={10}>
+            <A4Header local={localSettings} query={queryObject} />
+            <A4Body local={localSettings} query={queryObject} />
+          </SimpleGrid>
+          <A4Table local={localSettings} query={queryObject} />
         </Box>
-        <CloseButton
-          onClick={() =>
-            (document.querySelector(".alert").style = "display:none")
-          }
-          position="absolute"
-          right="8px"
-          top="8px"
-        />
-      </Alert>
-      <Box
-        color="black"
-        padding="3rem"
-        background={colorMode === "light" ? "whitesmoke" : "white"}
-        width="21cm"
-        height="29cm"
-      >
-        <SimpleGrid fontWeight="500" columns={2} spacing={10}>
-          <A4Header local={localSettings} query={queryObject} />
-          <A4Body local={localSettings} query={queryObject} />
-        </SimpleGrid>
-        <A4Table local={localSettings} query={queryObject} />
-      </Box>
-      <Button
-        onClick={() => window.print()}
-        _hover="none"
-        _focus="none"
-        shadow="lg"
-        position="fixed"
-        bottom="5px"
-        margin="4rem"
-        rightIcon={<ArrowForwardIcon />}
-        background="blue.300"
-        size="lg"
-      >
-        Print
-      </Button>
-    </Container>
-  );
+        <Button
+          onClick={() => window.print()}
+          _hover="none"
+          _focus="none"
+          shadow="lg"
+          position="fixed"
+          bottom="5px"
+          margin="4rem"
+          rightIcon={<ArrowForwardIcon />}
+          background="blue.300"
+          size="lg"
+        >
+          Print
+        </Button>
+      </Container>
+    );
+  }
+
+  return null;
 }
