225b309649e329889c9667cb56cbddc1c220c818
jack5341 <nedim.akar53411@gmail.com>
2021-03-20 17:04:27 +0100

    edit: removed empty icon folder

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9f39d61d0074c3eaa5a9";
+/******/ 	var hotCurrentHash = "55e77e01b66df7991d17";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
