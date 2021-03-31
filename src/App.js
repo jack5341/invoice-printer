95c8080a31419e4158ac8472325c6f0a748559b3
Nedim Akar <nedim.akar53411@gmail.com>
2021-03-31 13:46:55 +0200

    updated meta tags and scripts for production

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import { useEffect } from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react"
-import ReactGA from 'react-ga';
+import { useEffect } from "react";
+import { Helmet } from "react-helmet";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
 // Pages
-import Homepage from './pages/homepage'
-import Editor from './pages/editors'
-
-ReactGA.initialize("G-VTPVN058D5");
+import Homepage from "./pages/homepage";
+import Editor from "./pages/editors";
 
 function App() {
-
-  useEffect(() => ReactGA.pageview(window.location.pathname + window.location.search));
+  useEffect(() =>
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  );
 
   return (
     <ChakraProvider>
+      <Helmet>
+        <title>Invoice Printer</title>
+        <meta
+          name="description"
+          content="Free Fastest Invoice Printer by parsed Excel or Csv files. You should just upload your file and choose it."
+        />
+        <meta
+          name="keywords"
+          content="invoice printer, printer, invoice,automatic invoice printer, invoice printer by web app"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-VTPVN058D5"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || []; function gtag()
+            {dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', 'G-VTPVN058D5');
+            `,
+          }}
+        />
+        <script
+          data-ad-client="ca-pub-3442542443909627"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        ></script>
+
+        <script
+          data-ad-client="pub-3442542443909627"
+          async
+          src="https://pagead2.googlesyndication
+.com/pagead/js/adsbygoogle.js"
+        ></script>
+      </Helmet>
       <Router>
         <Switch>
           <Route component={Homepage} exact path="/" />
@@ -26,7 +58,7 @@ function App() {
         </Switch>
       </Router>
     </ChakraProvider>
-  )
+  );
 }
 
 export default App;
