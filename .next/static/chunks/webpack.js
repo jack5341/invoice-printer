677a392f9528d83d78fac92796f3342f7dc96190
Nedim <nedim.akar53411@gmail.com>
2021-04-29 08:43:56 +0200

    fix: links image error

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a89cadd32f34704a32ac"; }
+/******/ 		__webpack_require__.h = function() { return "cf7ec147e531433dc9e9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
